7d2a77f4afb2a6f7cc8decfaba18f5603f1f0417
Vikas <yourgithubemail@example.com>
2026-08-01 09:53:21 +0530

    Vikas Portfolio

---
+++ include/footer.php
@@ -0,0 +1,73 @@
+<footer class="ftco-footer ftco-section">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">About</h2>
+                    <p>Skilled in creating
+                        responsive, modern, and userfriendly websites using HTML, CSS, Bootstrap, and
+                        JavaScript. Experienced in front-end development and back-end
+                        technologies including PHP and MYSQL.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4 ml-md-4">
+                    <h2 class="ftco-heading-2">Links</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#home-section"><span class="icon-long-arrow-right mr-2"></span>Home</a></li>
+                        <li><a href="#about-section"><span class="icon-long-arrow-right mr-2"></span>About</a></li>
+                        <li><a href="#services-section"><span class="icon-long-arrow-right mr-2"></span>Services</a>
+                        </li>
+                        <li><a href="#experience-section"><span class="icon-long-arrow-right mr-2"></span>Experience</a>
+                        </li>
+                        <li><a href="#projects-section"><span class="icon-long-arrow-right mr-2"></span>Projects</a>
+                        </li>
+                        <li><a href="#contact-section"><span class="icon-long-arrow-right mr-2"></span>Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Services</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Design</a></li>
+                        <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Development</a></li>
+                        <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Business Strategy</a></li>
+                        <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Data Analysis</a></li>
+                        <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Graphic Design</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">C-575 Gali No.6 Haiderpur
+                                    Delhi 11008</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span
+                                        class="text">9990520777</span></a></li>
+                            <!-- <li><a href="#"><span class="icon icon-envelope"></span><span
+                                        class="text">  vikaskumar70085@gmail.com</span></a></li> -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright © 2026 Vikas Kumar. All rights reserved.</a>
+                        
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
